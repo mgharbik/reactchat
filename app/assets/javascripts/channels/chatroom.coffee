c0455869d0966cc74aeb056fe758184afcb505b6
Mohammed Gharbi <mohammed.gharbi@eventinc.de>
2017-10-31 17:11:10 +0100

    rendering new messages from React

--- app/assets/javascripts/channels/chatroom.coffee
+++
@@ -1,9 +0,0 @@
-$(document).on 'turbolinks:load', ->
-  App.chatroom = App.cable.subscriptions.create({
-    channel: 'ChatroomChannel',
-    id: $('#messages').attr('data-chatroom-id')
-  }, received: (data) ->
-    $('messages').append(data.message)
-    return
-  )
-  return
--- client/app/bundles/Chatroom/components/Chatroom.jsx
+++ client/app/bundles/Chatroom/components/Chatroom.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import MessagesList from './MessagesList'
+import update from 'immutability-helper'
 
 export default class Chatroom extends React.Component {
 
@@ -8,8 +9,27 @@ export default class Chatroom extends React.Component {
     super(props)
     let chatroom = JSON.parse(this.props.chatroom)
     this.state = {
-      messages: chatroom.messages
+      messages: chatroom.messages,
+      chatroomId: chatroom.id,
     }
+  }
+
+  updateMessages (message) {
+    const messages = update(this.state.messages,
+                              {$push: [message]})
+    this.setState({messages: messages})
+  }
+
+  componentDidMount () {
+    App.chatroom = App.cable.subscriptions.create({
+      channel: "ChatroomChannel",
+      id: this.state.chatroomId
+      },
+      {
+        received: function(data) {
+          this.updateMessages(JSON.parse(data.message))
+        }.bind(this)
+      });
   }
 
   render () {
