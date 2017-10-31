55d9c29667cf97d325857c0c01f53df9482b2e9d
Mohammed Gharbi <mohammed.gharbi@eventinc.de>
2017-10-31 17:27:31 +0100

    adding MessageForm component

--- client/app/bundles/Chatroom/components/Chatroom.jsx
+++ client/app/bundles/Chatroom/components/Chatroom.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import MessagesList from './MessagesList'
+import MessageForm from './MessageForm'
 import update from 'immutability-helper'
 
 export default class Chatroom extends React.Component {
@@ -36,6 +37,7 @@ export default class Chatroom extends React.Component {
     return (
       <div>
         <MessagesList messages={this.state.messages} />
+        <MessageForm chatroomId={this.state.chatroomId} />
       </div>
     )
   }
